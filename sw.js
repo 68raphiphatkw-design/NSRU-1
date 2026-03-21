8fa5958f71b40f220ac6381c7d619df23ee87273
68raphiphatkw-design <68raphiphat.kw@nsru.ac.th>
2026-03-21 14:38:44 +0700

    01
    01

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'nsru-portal-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request).catch(() => {
+          // If offline and request fails, return the cached index.html
+          if (event.request.mode === 'navigate') {
+            return caches.match('./index.html');
+          }
+        });
+      })
+  );
+});
